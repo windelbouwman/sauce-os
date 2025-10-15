6ba3fe7355dd0c0a1236424204722edf77f2ff43
Tom Smeets <tom@tsmeets.nl>
2025-10-15 19:24:47 +0200

    undo dynamic loading of sdl3

--- Libs/gfx/gfx.c
+++ Libs/gfx/gfx.c
@@ -26,28 +26,6 @@ typedef struct {
 
     // Down state
     uint8_t key_down[KEY_COUNT];
-
-    // Dynamic library
-    void *lib;
-    LIB_SYM(SDL_InitSubSystem);
-    LIB_SYM(SDL_Quit);
-    LIB_SYM(SDL_CreateWindow);
-    LIB_SYM(SDL_CreateRenderer);
-    LIB_SYM(SDL_DestroyRenderer);
-    LIB_SYM(SDL_DestroyTexture);
-    LIB_SYM(SDL_CreateTexture);
-    LIB_SYM(SDL_RenderPresent);
-    LIB_SYM(SDL_RenderTexture);
-    LIB_SYM(SDL_PollEvent);
-    LIB_SYM(SDL_UpdateTexture);
-    LIB_SYM(SDL_DelayPrecise);
-    LIB_SYM(SDL_SetTextureScaleMode);
-    LIB_SYM(SDL_OpenAudioDeviceStream);
-    LIB_SYM(SDL_GetAudioStreamDevice);
-    LIB_SYM(SDL_ResumeAudioDevice);
-    LIB_SYM(SDL_LockAudioStream);
-    LIB_SYM(SDL_UnlockAudioStream);
-    LIB_SYM(SDL_PutAudioStreamData);
 } Gfx_State;
 
 static Gfx_State gfx;
@@ -62,38 +40,14 @@ void gfx_init(const char *title, int width, int height) {
     if(gfx.window) return;
 
     // Load lib dynamically so that we don't have to link to sdl3 directly
-    gfx.lib = dlopen("libSDL3.so", RTLD_LOCAL | RTLD_NOW);
-
-#define LIB_LOAD(NAME) gfx.NAME = dlsym(gfx.lib, #NAME)
-    LIB_LOAD(SDL_InitSubSystem);
-    LIB_LOAD(SDL_Quit);
-    LIB_LOAD(SDL_CreateWindow);
-    LIB_LOAD(SDL_CreateRenderer);
-    LIB_LOAD(SDL_DestroyRenderer);
-    LIB_LOAD(SDL_DestroyTexture);
-    LIB_LOAD(SDL_CreateTexture);
-    LIB_LOAD(SDL_RenderPresent);
-    LIB_LOAD(SDL_RenderTexture);
-    LIB_LOAD(SDL_PollEvent);
-    LIB_LOAD(SDL_UpdateTexture);
-    LIB_LOAD(SDL_DelayPrecise);
-    LIB_LOAD(SDL_SetTextureScaleMode);
-    LIB_LOAD(SDL_OpenAudioDeviceStream);
-    LIB_LOAD(SDL_GetAudioStreamDevice);
-    LIB_LOAD(SDL_ResumeAudioDevice);
-    LIB_LOAD(SDL_LockAudioStream);
-    LIB_LOAD(SDL_UnlockAudioStream);
-    LIB_LOAD(SDL_PutAudioStreamData);
-#undef LIB_LOAD
-
-    gfx.SDL_InitSubSystem(SDL_INIT_EVENTS);
-    gfx.SDL_InitSubSystem(SDL_INIT_AUDIO);
-    gfx.SDL_InitSubSystem(SDL_INIT_VIDEO);
-    gfx.SDL_InitSubSystem(SDL_INIT_GAMEPAD);
+    SDL_InitSubSystem(SDL_INIT_EVENTS);
+    SDL_InitSubSystem(SDL_INIT_AUDIO);
+    SDL_InitSubSystem(SDL_INIT_VIDEO);
+    SDL_InitSubSystem(SDL_INIT_GAMEPAD);
     gfx.window_width = width;
     gfx.window_height = height;
-    gfx.window = gfx.SDL_CreateWindow(title, width, height, 0);
-    gfx.renderer = gfx.SDL_CreateRenderer(gfx.window, NULL);
+    gfx.window = SDL_CreateWindow(title, width, height, 0);
+    gfx.renderer = SDL_CreateRenderer(gfx.window, NULL);
 }
 
 void gfx_poll(void) {
@@ -101,7 +55,7 @@ void gfx_poll(void) {
 
     // Reset click events
     gfx.key_click_count = 0;
-    while (gfx.SDL_PollEvent(&event)) {
+    while (SDL_PollEvent(&event)) {
         switch (event.type) {
             case SDL_EVENT_QUIT: {
                 gfx_emit_key(KEY_APP_QUIT, true);
@@ -150,31 +104,31 @@ bool gfx_input_click(Key key) {
 
 void gfx_draw(int width, int height, uint8_t *pixels) {
     if(!gfx.texture ||width != gfx.texture_width || height != gfx.texture_height) {
-        if(gfx.texture) gfx.SDL_DestroyTexture(gfx.texture);
+        if(gfx.texture) SDL_DestroyTexture(gfx.texture);
         gfx.texture_width = width;
         gfx.texture_height = height;
-        gfx.texture = gfx.SDL_CreateTexture(gfx.renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, width, height);
-        gfx.SDL_SetTextureScaleMode(gfx.texture, SDL_SCALEMODE_NEAREST);
+        gfx.texture = SDL_CreateTexture(gfx.renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, width, height);
+        SDL_SetTextureScaleMode(gfx.texture, SDL_SCALEMODE_NEAREST);
     }
 
-    gfx.SDL_UpdateTexture(gfx.texture, NULL, pixels, width * 3);
-    gfx.SDL_RenderTexture(gfx.renderer, gfx.texture, NULL, NULL);
-    gfx.SDL_RenderPresent(gfx.renderer);
+    SDL_UpdateTexture(gfx.texture, NULL, pixels, width * 3);
+    SDL_RenderTexture(gfx.renderer, gfx.texture, NULL, NULL);
+    SDL_RenderPresent(gfx.renderer);
 }
 
 static void gfx_audio_callback(void *user, SDL_AudioStream *stream, int additional_amount, int total_amount) {
-    gfx.SDL_LockAudioStream(stream);
+    SDL_LockAudioStream(stream);
     uint32_t count = additional_amount / sizeof(int16_t);
     if (count > gfx.audio_count)
         count = gfx.audio_count;
     if (count > 0) {
-        gfx.SDL_PutAudioStreamData(stream, gfx.audio_buffer, count * sizeof(int16_t));
+        SDL_PutAudioStreamData(stream, gfx.audio_buffer, count * sizeof(int16_t));
         uint32_t remaining = gfx.audio_count - count;
         memmove(gfx.audio_buffer, gfx.audio_buffer + count,
                 remaining * sizeof(int16_t));
         gfx.audio_count = remaining;
     }
-    gfx.SDL_UnlockAudioStream(stream);
+    SDL_UnlockAudioStream(stream);
 }
 
 
@@ -186,12 +140,12 @@ void gfx_play(int count, int16_t *samples) {
             .channels = 1,
             .freq = 48000,
         };
-        gfx.audio_stream = gfx.SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &audio_spec, gfx_audio_callback, 0);
-        SDL_AudioDeviceID audio_device = gfx.SDL_GetAudioStreamDevice(gfx.audio_stream);
-        gfx.SDL_ResumeAudioDevice(audio_device);
+        gfx.audio_stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &audio_spec, gfx_audio_callback, 0);
+        SDL_AudioDeviceID audio_device = SDL_GetAudioStreamDevice(gfx.audio_stream);
+        SDL_ResumeAudioDevice(audio_device);
     }
 
-    gfx.SDL_LockAudioStream(gfx.audio_stream);
+    SDL_LockAudioStream(gfx.audio_stream);
 
     // Limit count
     if (count > ARRAY_COUNT(gfx.audio_buffer))
@@ -206,16 +160,16 @@ void gfx_play(int count, int16_t *samples) {
         gfx.audio_buffer[i] += samples[i];
     }
 
-    gfx.SDL_UnlockAudioStream(gfx.audio_stream);
+    SDL_UnlockAudioStream(gfx.audio_stream);
 }
 
 void gfx_sync(double interval) {
     uint64_t interval_us = interval * 1e6;
-    gfx.SDL_DelayPrecise(interval_us * 1000);
+    SDL_DelayPrecise(interval_us * 1000);
 }
 
 void gfx_quit(void) {
-    gfx.SDL_DestroyTexture(gfx.texture);
-    gfx.SDL_DestroyRenderer(gfx.renderer);
-    gfx.SDL_Quit();
+    SDL_DestroyTexture(gfx.texture);
+    SDL_DestroyRenderer(gfx.renderer);
+    SDL_Quit();
 }
